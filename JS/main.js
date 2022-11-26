f2154a25c9856061a695b63a22cf39aa5c392d0c
IvanLost <livan2000yanuary@gmail.com>
2022-11-26 18:22:12 +0300

    Масштабные исправления
    Удалил лишнее в cont.css. Чутка исправил новостную страницу, но не сильно. Добавил ссылки в nav. Заменил футер и хэдер на всех страницах реактом

--- JS/main.js
+++ JS/main.js
@@ -9,19 +9,19 @@ class header extends React.Component {
                 id: "nav",
                 className: "row"
             }, React.createElement("a", {
-                href: "#",
+                href: "index.html",
                 id: "logo"
             }, "\u041B\u043E\u0433\u043E"), React.createElement("a", {
-                href: "#",
+                href: "cont.html",
                 className: "navlink push"
             }, "\u041E \u043D\u0430\u0441"), React.createElement("a", {
                 href: "#",
                 className: "navlink"
             }, "\u041A\u0435\u0439\u0441\u044B"), React.createElement("a", {
-                href: "#",
+                href: "news.html",
                 className: "navlink"
             }, "\u041D\u043E\u0432\u043E\u0441\u0442\u0438"), React.createElement("a", {
-                href: "#",
+                href: "cont.html",
                 className: "navlink"
             }, "\u041A\u043E\u043D\u0442\u0430\u043A\u0442\u044B"))
         )
