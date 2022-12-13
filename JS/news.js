110196b66fa3972d93ccb810a3ae225fc4f6cba7
yanchiiick <118475793+yanchiiick@users.noreply.github.com>
2022-12-13 21:35:02 +0300

    добавила фото и пару вещей исправила

--- JS/news.js
+++ JS/news.js
@@ -1,10 +1,10 @@
-let ctx = document.querySelector('#myChart').getContext('2d');
+let ctx = document.querySelector( '#myChart').getContext('2d');
 let myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels:[9.09,11.09,14.09,17.09,21.09,22.09,24.09,26.09,27.09,30.09,2.10] ,
         datasets: [{
-            label:'Organig watchers' ,
+            label:'Органические охваты' ,
             data: [2029,8000,8020,10490,16000,34002,28849,30303,49490,48483,74242],
             backgroundColor:['white'] ,
             borderColor: [
@@ -13,6 +13,24 @@ let myChart = new Chart(ctx, {
             borderWidht: 4
         }]
     },
-    options: {}
-    })
+    options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+            display: true,
+            text: 'Даты',
+            color: '#911', }
+            },
+            y: {
+                display: true,
+                title: {
+                  display: true,
+                  text: 'Количество просмотров',
+                  color: '#191', }
+            }
+        }
+     } 
+})
 
